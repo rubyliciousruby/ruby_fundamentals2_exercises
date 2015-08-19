726f002137337da93a16022f598d45457496cff4
Ruby Li <rubyli@Ruby-Lis-MacBook-Pro-3.local>
2015-08-19 14:18:50 -0400

    Exercise 5

--- exercise5.rb
+++ exercise5.rb
@@ -1,6 +1,5 @@
 def temp_converter(f)
 	c = (f-32) * 5 / 9
-	#return "#{f} Fahrenheit is #{c} Celcius"
 	puts "#{f} Fahrenheit is #{c} Celcius"
 end
 print "Please enter any number of temperature in Fahrenheit. "
